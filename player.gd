7fbb81d7021b29a254beffdc8ca9b0b13646d7d2
Wellington Alves <contatoeipac@gmail.com>
2026-04-23 18:17:55 -0300

    movimento e gravidade

--- player.gd
+++ player.gd
@@ -14,3 +14,18 @@ func _input(event):
 		rotate_y(-event.relative.x * MOUSE_SENSIVITY)
 		camera.rotate_x(-event.relative.y * MOUSE_SENSIVITY)
 		camera.rotation.x = clamp(camera.rotation.x, -PI/2, PI/2)
+
+func _physics_process(delta): ## roda cada frame fisico do jogo, delta = tempo entre frames
+	if not is_on_floor(): ## verifica se o jogador está no chao, se não, aplica gravidade
+		velocity.y -= 9.8 * delta
+	var input_dir = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down") ##captura wasd
+	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized() ##converte a direção do input para o espaço do jogador
+	
+	if direction:
+		velocity.x = direction.x * SPEED
+		velocity.z = direction.z * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED) ##desacelera quando solta tecla
+		velocity.z = move_toward(velocity.z, 0, SPEED) 
+		
+	move_and_slide()## aplica movimento com detecção de colisão
